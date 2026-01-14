d4b581dceecaae6c4464e4ece075343fd39d2b6b
Sanchez <sanchez@sanchezs-mbp.lan>
2026-01-14 23:37:34 +0800

    first commit

---
+++ components/imageslider.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { motion } from "motion/react";
+import React from "react";
+import { ImagesSlider } from "./ui/images-slider";
+
+export function ImagesSliderDemo() {
+  const images = [
+    "/slider1.jpg",
+    "/slider2.jpg",
+    "/slider3.jpg",
+  ];
+  return (
+    <ImagesSlider className="h-[40rem]" images={images}>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -80,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.6,
+        }}
+        className="z-50 flex flex-col justify-center items-center"
+      >
+        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+          All about them bees
+        </motion.p>
+                <motion.p
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.3,
+            delay: 0.8,
+          }}
+          className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-50 dark:text-neutral-400"
+        >
+          Hi! I&apos;m Sonia, your friendly neighbourhood beekeeper.
+        </motion.p>
+        <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+          <span>Work With Me</span>
+          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+        </button>
+      </motion.div>
+    </ImagesSlider>
+  );
+}
