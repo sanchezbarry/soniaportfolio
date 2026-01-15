628cd922201f2e8b52470e25ceb3362a88867c0b
Sanchez <sanchez@Sanchezs-MacBook-Pro.local>
2026-01-15 11:28:42 +0800

    exports fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Hero } from "@/components/Hero";
+
 // import  FeaturesSectionDemo  from "@/components/features-section-demo-2"
-import { WorkshopForm } from "@/components/WorkshopForm";
-import { Services } from "@/components/Services";
-import { Gallery } from "@/components/Gallery";
+import { WorkshopForm } from "../components/WorkshopForm";
+import { Services } from "../components/Services";
+import { Gallery } from "../components/Gallery";
 import 'material-symbols/outlined.css';
+import { Hero } from "../components/Hero";
 
 export default function Home() {
   return (
